5efec4c39926fb5183a2f12775b413c7abf74c2e
Bruno Ferreira Rodrigues <130567518+brunofrodrigues@users.noreply.github.com>
2023-10-25 10:52:00 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Login</title>
+</head>
+
+    <fieldset>
+        <form action="" method="get">
+          <label for="email">Email:</label><br>
+          <input name="email" type="text" id="email" value=""><br>
+        
+          <label for="senha">Senha:</label><br>
+          <input name="senha" type="password" id="senha" value=""><br>
+
+    </fieldset>
+
+    <button type="submit" id="">Logar</button>
+        </form>
