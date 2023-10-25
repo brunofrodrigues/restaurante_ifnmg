04b39fe858135fbc578c1c84e88b281ff947c741
Bruno Ferreira Rodrigues <130567518+brunofrodrigues@users.noreply.github.com>
2023-10-25 10:37:19 -0300

    Add files via upload

---
+++ inc/header.inc.php
@@ -0,0 +1,11 @@
+<header>
+    <div>
+        <h2>Projeto Restaurante - IFNMG</h2>
+            <ul>
+                <li><a href="index.php">Página principal</a></li>
+                <li><a href="cadastrar.html">Cadastrar</a></li>
+                <li></li>
+            </ul>
+    </div>
+
+</header> 
